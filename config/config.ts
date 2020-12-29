a48440582a9869be72c938bf1251aa912a7d24c6
zhenglipeng <zhenglipeng@sensetime.com>
2020-12-29 14:39:50 +0800

    publicPath修改

--- config/config.ts
+++ config/config.ts
@@ -7,6 +7,8 @@ import routes from './routes';
 const { REACT_APP_ENV } = process.env;
 
 export default defineConfig({
+  publicPath: '/pupy/',
+  base: 'pupy/',
   hash: true,
   antd: {},
   dva: {
